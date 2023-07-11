28b88c405b02d0f757442c660c6965c50360ecd1
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2023-07-12 00:37:12 +0700

    redirect to dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,86 @@
-// @mui
-import { styled } from '@mui/material/styles';
-// layouts
-import Layout from '../layouts';
-// components
-import Page from '../components/Page';
-// sections
-import {
-  HomeHero,
-  HomeMinimal,
-  HomeDarkMode,
-  HomeLookingFor,
-  HomeColorPresets,
-  HomePricingPlans,
-  HomeAdvertisement,
-  HomeCleanInterfaces,
-  HomeHugePackElements,
-} from '../sections/home';
+// // @mui
+// import { styled } from '@mui/material/styles'
+// // layouts
+// import Layout from '../layouts'
+// // components
+// import Page from '../components/Page'
+// // sections
+// import {
+//   HomeHero,
+//   HomeMinimal,
+//   HomeDarkMode,
+//   HomeLookingFor,
+//   HomeColorPresets,
+//   HomePricingPlans,
+//   HomeAdvertisement,
+//   HomeCleanInterfaces,
+//   HomeHugePackElements,
+// } from '../sections/home'
+
+// // ----------------------------------------------------------------------
+
+// const ContentStyle = styled('div')(({ theme }) => ({
+//   overflow: 'hidden',
+//   position: 'relative',
+//   backgroundColor: theme.palette.background.default,
+// }))
+
+// // ----------------------------------------------------------------------
+
+// HomePage.getLayout = function getLayout(page: React.ReactElement) {
+//   return <Layout variant="main">{page}</Layout>
+// }
+
+// // ----------------------------------------------------------------------
+
+// export default function HomePage() {
+//   return (
+//     <Page title="The starting point for your next project">
+//       <HomeHero />
+
+//       <ContentStyle>
+//         <HomeMinimal />
+
+//         <HomeHugePackElements />
+
+//         <HomeDarkMode />
+
+//         <HomeColorPresets />
+
+//         <HomeCleanInterfaces />
+
+//         <HomePricingPlans />
+
+//         <HomeLookingFor />
+
+//         <HomeAdvertisement />
+//       </ContentStyle>
+//     </Page>
+//   )
+// }
+
+import { useEffect } from 'react'
+// next
+import { useRouter } from 'next/router'
+// config
+import { PATH_AFTER_LOGIN } from '../config'
 
 // ----------------------------------------------------------------------
 
-const ContentStyle = styled('div')(({ theme }) => ({
-  overflow: 'hidden',
-  position: 'relative',
-  backgroundColor: theme.palette.background.default,
-}));
+export default function Index() {
+  const { pathname, replace, prefetch } = useRouter()
 
-// ----------------------------------------------------------------------
+  useEffect(() => {
+    if (pathname === '/') {
+      replace(PATH_AFTER_LOGIN)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname])
 
-HomePage.getLayout = function getLayout(page: React.ReactElement) {
-  return <Layout variant="main">{page}</Layout>;
-};
+  useEffect(() => {
+    prefetch(PATH_AFTER_LOGIN)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-// ----------------------------------------------------------------------
-
-export default function HomePage() {
-  return (
-    <Page title="The starting point for your next project">
-      <HomeHero />
-
-      <ContentStyle>
-        <HomeMinimal />
-
-        <HomeHugePackElements />
-
-        <HomeDarkMode />
-
-        <HomeColorPresets />
-
-        <HomeCleanInterfaces />
-
-        <HomePricingPlans />
-
-        <HomeLookingFor />
-
-        <HomeAdvertisement />
-      </ContentStyle>
-    </Page>
-  );
+  return null
 }
